9587d724eb46deec2aa0b34f26e86ac1e54d7cba
rodmasalskiene <rodolfowm707@gmail.com>
2022-11-27 22:35:23 -0200

    V 0.3 - adicionado funções para os botões 1 e 2

---
+++ index.js
@@ -0,0 +1,73 @@
+/*
+document.getElementById("tool-1").onclick = function F1_uppercase(){
+    var texto = document.getElementById("entertext").value;
+    document.getElementById("outtext").value = texto.toUpperCase();
+}
+
+document.getElementById("tool-2").onclick = function F2_lowecase(){
+    var texto = document.getElementById("entertext").value;
+    document.getElementById("outtext").value = texto.toLowerCase();
+}
+
+document.getElementById("tool-3").onclick = function F3_changecolor(){
+    var texto = document.getElementById("entertext").value;
+    document.getElementById("outtext").value = 
+}
+
+*/
+
+var pallet = [];
+
+function colorpallet(){
+
+
+}
+
+// Ela receberia uma div que eu criei no html e deixei como display none
+//  Essa div seria a paleta de cores
+// Mas nessa função ao chamar o elemento seu display teria que ser alterado de none pra -float?-- para que ele possa aparecer
+
+
+/* V. 0.1
+
+function colorpallet(){
+    let r = 0;
+    let g = 0;
+    let b = 0;
+        if (condition) {
+            
+        }
+}
+
+*/
+
+/* V. 0.2
+
+var pallet = [];
+
+function colorpallet(){
+    var options = document.getElementById("tool-3").onclick = document.getElementById("pallet-options");
+    document.getElementById("pallet-options").style.display = flex;
+}
+
+*/
+
+/* V. 0.3   -- Consegui abrir o menu de cores
+
+onclick="document.getElementById('pallet-options').style.display = 'flex'"
+
+*/
+
+/* V. 0.4
+
+document.getElementById("tool-3").onclick = document.getElementById('pallet-options').style.display='flex';
+
+*/
+
+/* V. 0.5
+
+
+
+*/
+
+
